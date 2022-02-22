3593d23b425d546abe8c6e5933a7d5480e8115e3
Taras Kulian <kuliantaras@gmail.com>
2022-02-22 10:01:46 +0200

    add another solution

--- task_2.js
+++ task_2.js
@@ -1,10 +1,32 @@
 //Написати функцію, яка поверне знайде чи входить шукана стрічка у задану
+
 const str = 'Javascript (JS) is a scripting languages, primarily used on the Web. It is used to enhance HTML pages and is commonly found embedded in HTML code';
 const word = 'Javascript';
-console.log(`Here is string:\n${str}\nHere is word that we are searching for: ${word}\nResult: ${str.includes(word)}`);
+
+//Варіант перший
+// console.log(`Here is string:\n${str}\nHere is word that we are searching for: ${word}\nResult: ${str.includes(word)}`);
+
+//Варіант другий (не оптимізований)
+let flag;
+let temp;
+for (let i = 0; i < str.length; i++) {
+    temp = i + 1;
+    flag = true;
+    for (let j = 0; j < word.length; j++) {
+        if ((i >= str.length)||(word[j] !== str[i++])) {
+            flag = false;
+            break;
+        }
+    }
+    if (flag) break;
+    else {
+        i = temp;
+    }
+}
+console.log(`Here is string:\n${str}\nHere is word that we are searching for: ${word}\nResult: ${flag}`)
 
 
-// //Написати функцію, яка поверне усі перестановки слова
+//Написати функцію, яка поверне усі перестановки слова
 // function findPermutations(word) {
 //     if (word.length < 2 ){
 //         return word
